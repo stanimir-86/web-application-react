394ac89f575d069e17c26653167603e4af09f064
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-06 18:32:54 +0300

    Create database.js

---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/reactapplication';
+
+module.exports = async (app) => {
+    try {
+        await mongoose.connect(CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Database connected');
+
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
