ee8f709a0e6befe47a90f35eed5e1fc579591a3d
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-08 13:54:03 +0300

    Update userService.js

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,51 +1,70 @@
-const User = require('../models/User.js');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User.js');
 
 const JWT_SECRET = 'dfjdfj45drdfhk!@tsrer';
 
-async function register(username, password) {
+async function register(email, username, password) {
 
-    const existing = await User.findOne({ username })
-        .collation({ locale: 'en,strength:2' });
+    console.log(email, username, password);
+
+    const existing = await User.findOne({ email })
+
 
     if (existing) {
-        throw new Error('Username is taken!');
+        throw new Error('Email is taken!');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
         username,
+        email,
         hashedPassword
     });
-    const token = createSesion(user);
+    return createSesion(user);
 
-    return token;
-};
-
-async function login() {
 
 };
 
-function verifyToken() {
+async function login(email, password) {
+    const user = await User.findOne({ email });
 
+    if (!user) {
+        throw new Error('Invalid email or password!');
+    }
+    const match = await bcrypt.compare(password, user.hashedPassword);
+    if (!match) {
+        throw new Error('Invalid email or password!');
+    }
+    return createSesion(user);
 };
 
-function createSesion({ _id, username }) {
+function createSesion(user) {
     const payload = {
-        _id,
-        username,
+        _id: user._id,
+        username: user.username,
+        email: user.email,
     };
 
-    const token = jwt.sign(payload, JWT_SECRET);
-    return token;
+    return {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        accesToken: jwt.sign(payload, JWT_SECRET),
+    }
+
+};
+function verifyToken(token) {
+    try {
+        return jwt.verify(token, JWT_SECRET);
+    } catch (err) {
+        throw new Error('Invalid acces token!');
+    }
 };
 
 module.exports = {
     register,
     login,
     verifyToken,
-
-
 }
