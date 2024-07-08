e479e54a501745b31a6d1991d25e4751a3df1833
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-08 13:54:00 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,22 @@
 const { Schema, model } = require('mongoose');
 
+const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const userSchema = new Schema({
-    username: { type: String, required: true },
+    email: {
+        type: String, required: true, unique: true, validate: {
+            validator: (value) => emailRegex.test(value),
+            message: "Invalid emali!",
+        }
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [3, 'Username must be at least three charecters!'],
+        maxlength: [10, 'Username must be at least ten charecters!'],
+
+    },
     hashedPassword: { type: String, required: true },
     unique: true,
     minlength: [3, 'Username must be at least three charecters!'],
@@ -10,7 +24,7 @@ const userSchema = new Schema({
 
 });
 
-userSchema.index({ username: 1 }, {
+userSchema.index({ email: 1 }, {
     collation: {
         locale: 'en',
         strength: 2
