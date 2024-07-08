ee33af4186ab04e48f727e8acbd12038e287c986
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-08 14:45:46 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const app = express();
-//cors is needed
+const cors=require('./middlewares/cors.js');
 const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/reactapplication';
 const { mongoose } = require('mongoose');
-// const databaseConfig = require('./config/database.js');
-// const routesConfig = require('./config/routes.js');
-
 const router = require('./config/routes.js');
-//sesion
+const session = require('./middlewares/session.js');
+mongoose.set('strictQuery', true);
+
+
+
 const startDB = () => mongoose.connect(CONNECTION_STRING);
+
 start();
 
 async function start() {
     startDB();
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
-    app.use(router)
-    // routesConfig(app);
+    app.use(cors());
+    app.use(session());
 
+    
+    
+    app.use(router);
     app.listen("3030", () => console.log('Server listening on port 3030'));
 }
