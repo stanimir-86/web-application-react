f22d8f5b8d810e8d876af17e33261014b0702321
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-08 14:45:50 +0300

    Create session.js

---
+++ server/middlewares/session.js
@@ -0,0 +1,16 @@
+const { verifyToken } = require('../services/userService.js');
+
+module.exports = () => (req, res, next) => {
+    const token = req.headers['x-authorization'];
+
+    if (token) {
+        try {
+            const payload = verifyToken(token);
+            req.user = payload;
+            req.token = token;
+        } catch (error) {
+            return res.status(401).json({ message: 'Invalid authorization token!' });
+        }
+    }
+    next();
+};
