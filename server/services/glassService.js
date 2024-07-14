d904e2a4a943b6bbf1505952e989893fa3082f61
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-14 17:21:52 +0300

    Update glassService.js

--- server/services/glassService.js
+++ server/services/glassService.js
@@ -15,7 +15,7 @@ async function getById(id) {
 
 async function create(data) {
     return Sunglasses.create(data);
-}
+};
 async function getMyLikes(id) {
     const glasses = await Sunglasses.find({});
     let result = [];
@@ -25,16 +25,50 @@ async function getMyLikes(id) {
         }
     });
     return result;
+};
+
+async function update(id, glasses) {
+
+    const existing = await Sunglasses.findById(id);
+    existing.brand = glasses.brand;
+    existing.model = glasses.model;
+    existing.price = glasses.price;
+    existing.color = glasses.color;
+    existing.lensMaterial = glasses.lensMaterial;
+    existing.frameMaterial = glasses.frameMaterial;
+    existing.polarized = glasses.polarized;
+    existing.uvProtection = glasses.uvProtection;
+    existing.stock = glasses.stock;
+    existing.description = glasses.description;
+    existing.images = glasses.images;
+    existing.dateAdded = glasses.dateAdded;
+
+    return existing.save();
+};
+
+async function deleteLikes(id, userId) {
+    const existing = await Sunglasses.findById(id);
+    existing.likes = existing.likes.filter(x => x != userId);
+    return existing.save();
+};
+
+async function deleteById(id) {
+    return Sunglasses.findByIdAndDelete(id);
+};
+
+async function likeSunglasses(glassesId, userId) {
+    const existing = await Sunglasses.findById(glassesId);
+    existing.likes.push(userId);
+    return existing.save();
 }
-
-a
-
 module.exports = {
     getAll,
     getByUserId,
     getById,
     create,
     getMyLikes,
-
-
+    update,
+    deleteLikes,
+    deleteById,
+    likeSunglasses,
 };
