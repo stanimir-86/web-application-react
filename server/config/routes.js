48d5d8f0a21c3aa0b9cec9a898972391a898b5b1
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-14 17:03:31 +0300

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,5 @@
 const authController = require("../controllers/authController.js");
+const sunglassController = require("../controllers/sunglassController.js");
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
@@ -6,5 +7,5 @@ router.get('/', (req, res) => {
     res.json({ message: "Service in operation" })
 });
 router.use('/auth', authController);
-
+router.use('/sunglasses', sunglassController);
 module.exports = router;
