eef2577d02dee211fd032afd94c8e59b216c7105
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-16 09:12:03 +0300

    Update sunglassController.js

--- server/controllers/sunglassController.js
+++ server/controllers/sunglassController.js
@@ -1,4 +1,17 @@
 const sunglassController = require('express').Router();
 
+const { getAll } = require('../services/glassService.js');
+
+sunglassController.get('/', async (req, res) => {
+
+    try {
+        const glasess = await getAll();
+        res.status(200).json(glasess);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+
 
 module.exports = sunglassController;
