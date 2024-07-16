127375935cab16ff268bcccdda4e8375805a9fe6
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-07-16 17:07:36 +0300

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,22 +1,53 @@
+const { register, login } = require('../services/userService.js');
+
 const authController = require('express').Router();
 
 
-authController.get('/register', (req, res) => {
-    // res.render('register', {
-    //     title: 'Register Page'
-    // });
-    res.json({ message: "Register" })
+authController.post('/register', async (req, res) => {
+    try {
+        if (req.body.password !== req.body.repass) {
+            throw new error('Password dismatch!');
+        }
+        const token = await register(
+            req.body.email,
+            req.body.username,
+            req.password,
+        )
+        if (!token) {
+            throw new error('Unable to register with this credential!!!');
+        }
+        res.status(201).json(token);
+        res.end();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 
 });
 
-authController.post('/register', async (req, res) => {
+authController.post('/login', async (req, res) => {
 
-    // const token = await register(req.body.username, req.body.password);
-    const token = (req.body);
-    console.log(token);
-    res.json({ message: "Register Post request OK" })
+    try {
+        const token = await login(req.body.email, req.body.password);
+        if (!token) {
+            throw new error('Unable to register with this credential!!!');
+        }
+        res.status(201).json(token);
+        res.end();
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message });
+    }
+});
 
-    // res.redirect('/auth/register')
+authController.get('/user', async (req, res) => {
+    try {
+        const user = req.user;
+        res.status(200).json(user);
+        res.end();
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message })
+    }
 });
 
 module.exports = authController;
