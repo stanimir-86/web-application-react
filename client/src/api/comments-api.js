5c8f18c4785b5f8e5b16efc8be8d03265e4cb6c0
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-08-01 14:32:34 +0300

    Create comments-api.js

---
+++ client/src/api/comments-api.js
@@ -0,0 +1,17 @@
+import requester from "./requester.js";
+
+const BASE_URL = 'http://localhost:3030/sunglasses';
+const buildUrl = (sunglassesId) => `${BASE_URL}/${sunglassesId}/comments`;
+
+const create = async (sunglassesId, username, text) => requester.post(buildUrl(sunglassesId), { username, text });
+
+const getAll = async (sunglassesId) => {
+    const result = await requester.get(buildUrl(sunglassesId));
+    const comments = Object.values(result);
+    return comments;
+
+}
+export default {
+    create,
+    getAll,
+}
