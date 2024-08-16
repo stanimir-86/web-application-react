aada1291fdf04ba1b54bb6576191052393095a1f
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-08-16 18:33:00 +0300

    Clear form after submit

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -13,8 +13,8 @@ export function useForm(initialValues, submitCallback) {
 
     const submitHandler = (e) => {
         e.preventDefault();
-
         submitCallback(values);
+        setValues(initialValues);
     }
     return {
         values,
